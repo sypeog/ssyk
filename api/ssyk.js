b376095fd49bfb61b464a55e0d4bce0b6551b943
sypeog <129770819+sypeog@users.noreply.github.com>
2026-05-12 14:56:30 +0200

    Update ssyk.js

--- api/ssyk.js
+++ api/ssyk.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         "X-Title": "SSYK App"
       },
       body: JSON.stringify({
-        model: "mistralai/mistral-7b-instruct:free",
+        model: "nousresearch/nous-hermes-2-mixtral-8x7b-dpo:free",
         temperature: 0.2,
         messages: [
           {
